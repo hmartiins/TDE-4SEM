dd97b76e60bcfa1253d1c2d8e40dfcd87a6c03c4
hmartiins <hmartins224@gmail.com>
2023-10-17 19:38:31 -0300

    feat: add group hover in information card

--- src/components/InformationCard.tsx
+++ src/components/InformationCard.tsx
@@ -5,8 +5,8 @@ interface InformationCardProps {
 
 export function InformationCard({ text, title }: InformationCardProps) {
   return (
-    <article className="w-full flex gap-[18px]">
-      <div className="w-[2px] bg-blue-500"></div>
+    <article className="group w-full flex gap-[18px]">
+      <div className="w-[2px] bg-gray-500 group-hover:bg-blue-500 transition-all duration-700"></div>
 
       <div className="flex flex-col gap-[15px] py-2">
         <b className="text-white font-black text-lg">{title}</b>
