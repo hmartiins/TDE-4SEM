434a158f96e39ba2f2b9b851f92cde7d931c1178
hmartiins <hmartins224@gmail.com>
2023-10-17 20:28:01 -0300

    feat: add text in information

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -2,12 +2,12 @@ export function Information() {
   return (
     <section className="max-w-full overflow-hidden mx-5">
       <h3 className="text-center font-black text-sm uppercase text-white mt-14">
-        Importância da Análise
+        Educação no Brasil
       </h3>
 
       <p className="text-white text-center font-black leading-[130%] text-[28px] mt-5">
-        Move fast, stay aligned, and build better -{" "}
-        <span className="text-blue-500">together</span>
+        Invista com sabedoria, eduque com coragem{" "}
+        <span className="text-blue-500">coragem</span>
       </p>
 
       <p className="mt-5 text-center text-lg text-white leading-7">
